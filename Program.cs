e28915e6378fb380749c511541760f8bfd15a214
Adam Miller <theadammiller56@gmail.com>
2022-06-14 08:49:26 -0500

    Dev

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_SourceControl_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            function();
+        }
+        private static void function()
+        {
+            return;
+        }
+    }
+}
