1c089c443788f6c0e33f30b8503e31a56fbc06cd
SeanR <s.m.reagan22@gmail.com>
2022-06-14 12:20:53 -0500

    div by three and every fifth

--- Program.cs
+++ Program.cs
@@ -7,11 +7,37 @@ namespace C_Sharp_SourceControl_2
     {
         public static void Main(string[] args)
         {
-            function();
+            int[] array2 = new int[] { 1, 3, 5, 7, 9 };
+            foreach(int i in divByThree(array2)){
+            Console.WriteLine(i);
+            }
+            foreach(int i in everyFifth(array2)){
+            Console.WriteLine(i);
+            }
         }
-        private static void function()
+        public static List<int> divByThree(int[] array)
         {
-            return;
+            List<int> result = new List<int>();
+            foreach(int i in array){
+                if(i%3 == 0){
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+        public static List<int> everyFifth(int[] array){
+            int count = 1;
+            List<int> result = new List<int>();
+            foreach(int i in array){
+                if (count%5 == 0){
+                    result.Add(i);
+                    count++;
+                }
+                else{
+                    count++;
+                }
+            }
+            return result;
         }
     }
 }
