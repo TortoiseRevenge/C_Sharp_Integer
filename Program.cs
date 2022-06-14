239f195fe286866c3b6ba6298f3f109254640cc1
Divya Krishnan <divya.brookfield@gmail.com>
2022-06-14 12:27:13 -0500

    Sum of integers

--- Program.cs
+++ Program.cs
@@ -7,11 +7,23 @@ namespace C_Sharp_SourceControl_2
     {
         public static void Main(string[] args)
         {
-            function();
+             int[] nums = {1, 3, 5, 7, 9 };
+             sum(nums);
+             Console.WriteLine(sum(nums));
+
         }
         private static void function()
         {
             return;
+        }
+        private static int sum(int[] numbers)
+        {
+            int sum=0;
+               for (int i =0; i<numbers.Length; i++){
+                    sum+=numbers[i];
+               }
+               return sum;
+
         }
     }
 }
